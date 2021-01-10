7b78391613705c6b361291b098103437f31d11de
Lukasz Malucha <lucasmalucha@gmail.com>
2021-01-10 13:44:43 +0000

    data

---
+++ resources/js/world_events.js
@@ -0,0 +1,238 @@
+export default
+
+[
+  {
+    "title": "3 days of Darkness",
+    "message": "My daughter, the three days of darkness are about to arrive",
+    "tag": "Cataclysms",
+    "order": "1",
+    "message": "2016-05-02"
+  },
+  {
+    "title": "France Earthquake",
+    "message": "After France it will be Rome where there will be a great earthquake",
+    "tag": "Cataclysms",
+    "order": "2",
+    "message": "2016-06-18"
+  },
+  {
+    "title": "Italy Earthquake",
+    "message": "After France it will be Rome where there will be a great earthquake",
+    "tag": "Cataclysms",
+    "order": "3",
+    "message": "2016-06-18"
+  },
+  {
+    "title": "Vatican Pain",
+    "message": "The Vatican will suffer great pain",
+    "tag": "Catholic Church",
+    "order": "4",
+    "message": "2016-06-18"
+  },
+  {
+    "title": "Russia prepares to war",
+    "message": "Listen to the winds of war. Russia is preparing",
+    "tag": "Wars",
+    "order": "1",
+    "message": "2016-06-18"
+  },
+  {
+    "title": "Plagues of Rome",
+    "message": "Pestilence, misery and smallpox will fall on Rome",
+    "tag": "Europe",
+    "order": "1",
+    "message": "2016-06-18"
+  },
+  {
+    "title": "Short Pontificate",
+    "message": " pray for the Pope, because his pontificate will be short",
+    "tag": "Catholic Church",
+    "order": "1",
+    "message": "2016-06-18"
+  },
+  {
+    "title": "World war is coming",
+    "message": "Our Lady also added that a great World War is brewing",
+    "tag": "Wars",
+    "order": "1",
+    "message": "2016-07-26"
+  },
+  {
+    "title": "Brazil cup of bitterness",
+    "message": "The Earth is about to tremble, Brazil will soon drink from the bitter cup",
+    "tag": "World",
+    "order": "5",
+    "message": "2016-07-30"
+  },
+  {
+    "title": "Pray for Chile and America",
+    "message": " I ask you to pray for Chile and America",
+    "tag": "World",
+    "order": "5",
+    "message": "2016-08-02"
+  },
+  {
+    "title": "Pray for America",
+    "message": " I ask you to pray for Chile and America",
+    "tag": "World",
+    "order": "5",
+    "message": "2016-08-02"
+  },
+  {
+    "title": "3rd Horseman",
+    "message": "Soon my Son will reveal the third Seal (famine, death, despair)",
+    "tag": "Plagues",
+    "order": "5",
+    "message": "2016-08-16"
+  },
+  {
+    "title": "Strong Pain in France",
+    "message": "Pray for France because it will face yet another strong pain",
+    "tag": "Plagues",
+    "order": "5",
+    "message": "2016-08-23"
+  },
+  {
+    "title": "Bad times for Italy and Rome",
+    "message": "Pray for Italy especially for Rome because the forecasts are not good",
+    "tag": "Plagues",
+    "order": "5",
+    "message": "2016-08-23"
+  },
+  {
+    "title": "Chile earthquake",
+    "message": "pray for Chile, the earth will tremble again and volcanoes will start to erupt one after the other in all parts of the world",
+    "tag": "Plagues",
+    "order": "5",
+    "message": "2016-08-23"
+  },
+  {
+    "title": "World Vulcanos erupt",
+    "message": "pray for Chile, the earth will tremble again and volcanoes will start to erupt one after the other in all parts of the world",
+    "tag": "Cataclysms",
+    "order": "5",
+    "message": "2016-08-23"
+  },
+    {
+    "title": "Chile earthquake",
+    "message": "Pray for the Japan and Chile where the earth will soon tremble",
+    "tag": "Cataclysms",
+    "order": "5",
+    "message": "2016-09-19"
+  },
+  {
+    "title": "Japan earthquake",
+    "message": "Pray for the Japan and Chile where the earth will soon tremble",
+    "tag": "Cataclysms",
+    "order": "5",
+    "message": "2016-09-19"
+  },
+  {
+    "title": "Cross on the sky",
+    "message": "A great sign will be a cross visible throughout the world",
+    "tag": "Signs",
+    "order": "5",
+    "message": "2016-09-22"
+  },
+  {
+    "title": "Vulcano in Mexico awakened",
+    "message": "pray for Mexico where the volcano has awakened",
+    "tag": "Cataclysms",
+    "order": "5",
+    "message": "2016-09-29"
+  },
+  {
+    "title": "Italy Earthqake",
+    "message": " pray for Italy where there will be a strong earthquake",
+    "tag": "Cataclysms",
+    "order": "5",
+    "message": "2016-09-29"
+  },
+  {
+    "title": "Evil in Columbia",
+    "message": "pray for Colombia where the forces of evil are awakening",
+    "tag": "Wars",
+    "order": "5",
+    "message": "2016-09-29"
+  },
+    {
+    "title": "Vulcanoes causing earthquakes ",
+    "message": "due to strong eruptions of a volcano, the earth will still tremble",
+    "tag": "Cataclysm",
+    "order": "5",
+    "message": "2016-10-01"
+  },
+  {
+    "title": "Church's bad agreements",
+    "message": "Beware of the agreements that will be signed by the church, they are not from God",
+    "tag": "Catholic Church",
+    "order": "5",
+    "message": "2016-10-01"
+  },
+  {
+    "title": "Evil in Canada",
+    "message": "Pray for Canada where the forces of evil are preparing and pray for Central America where there will be a major earthquake ",
+    "tag": "Wars",
+    "order": "5",
+    "message": "2016-10-04"
+  },
+  {
+    "title": "Central America earthquake",
+    "message": "Pray for Canada where the forces of evil are preparing and pray for Central America where there will be a major earthquake ",
+    "tag": "Cataclysm",
+    "order": "5",
+    "message": "2016-10-04"
+  },
+  {
+    "title": "Antichrist takes the throne",
+    "message": "The evil one is about to take the throne inside the church in place of my Son and soon the Eucharist will disappear",
+    "tag": "Signs",
+    "order": "5",
+    "message": "2016-10-08"
+  },
+  {
+    "title": "Eucharist will disappear",
+    "message": "The evil one is about to take the throne inside the church in place of my Son and soon the Eucharist will disappear",
+    "tag": "Signs",
+    "order": "5",
+    "message": "2016-10-08"
+  },
+  {
+    "title": "Jesus comes back",
+    "message": "Jesus will come back and everyone will see him",
+    "tag": "Signs",
+    "order": "5",
+    "message": "2016-10-08"
+  },
+   {
+    "title": "Chile earthquake",
+    "message": "Pray for Chile where the earth will tremble vigorously and pray for Japan and America where the scourge will continue for purification",
+    "tag": "Cataclysms",
+    "order": "5",
+    "message": "2016-10-08"
+  },
+   {
+    "title": "Japan's plague",
+    "message": "Pray for Chile where the earth will tremble vigorously and pray for Japan and America where the scourge will continue for purification",
+    "tag": "Plagues",
+    "order": "5",
+    "message": "2016-10-08"
+  },
+   {
+    "title": "America's plague",
+    "message": "Pray for Chile where the earth will tremble vigorously and pray for Japan and America where the scourge will continue for purification",
+    "tag": "Plagues",
+    "order": "5",
+    "message": "2016-10-08"
+  },
+  {
+    "title": "Disease with no cure",
+    "message": "Pray so that we can eradicate a disease for which there will be no cure",
+    "tag": "Cataclysm",
+    "order": "5",
+    "message": "2016-10-10"
+  },
+
+
+
+]
